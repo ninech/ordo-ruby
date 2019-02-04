153ac21da5875204639c253c4650a958b2c8df76
RadicM <milosrt2011@gmail.com>
2019-02-04 15:32:55 +0100

    - refactored orderable spec

--- spec/ordy/orm/active_record/orderable_spec.rb
+++ spec/ordy/orm/active_record/orderable_spec.rb
@@ -2,89 +2,147 @@ require 'spec_helper'
 require './spec/support/orm/active_record/active_record_test_db'
 
 describe Ordy::Orm::ActiveRecord::Orderable do
+
   describe '#order_by' do
+
     context 'columns' do
-      it 'should order name asc' do
-        expect(User.order_by(name: :asc).pluck(:name)).to eq(%w(axample demo example))
-        expect(User.order_by('name-asc').pluck(:name)).to eq(%w(axample demo example))
+      let(:base_query) { User }
+      let(:order) { base_query.order_by(order_by) }
+      let(:result) { order.pluck(:name) }
+
+      context 'order by name: :asc' do
+        let(:order_by) { { name: :asc } }
+        specify { expect(result).to eq(%w(axample demo example)) }
       end
 
-      it 'should order name desc' do
-        expect(User.order_by(name: :desc).pluck(:name)).to eq(%w(example demo axample))
-        expect(User.order_by('name-desc').pluck(:name)).to eq(%w(example demo axample))
+      context 'order by name-asc' do
+        let(:order_by) { 'name-asc' }
+        specify { expect(result).to eq(%w(axample demo example)) }
       end
 
-      it 'should not change base query' do
-        query = User.where(name: 'example')
-        query.order_by(name: :desc).pluck(:name)
-        expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+      context 'order by name: :desc' do
+        let(:order_by) { { name: :desc } }
+        specify { expect(result).to eq(%w(example demo axample)) }
+      end
+
+      context 'order by name-desc' do
+        let(:order_by) { 'name-desc' }
+        specify { expect(result).to eq(%w(example demo axample)) }
+      end
+
+      context 'base query immutability' do
+        it 'should not change base query' do
+          query = base_query.where(name: 'example')
+          query.order_by(name: :desc).pluck(:name)
+          expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+        end
       end
     end
 
     context 'associations' do
-      it 'should order by comment content asc' do
-        expect(User.order_by(comments: :asc).pluck(:id)).to eq([3, 2, 1])
+      let(:base_query) { User }
+      let(:order) { base_query.order_by(order_by) }
+      let(:result) { order.pluck(:id) }
+
+      context 'order by name: :asc' do
+        let(:order_by) { { comments: :asc } }
+        specify { expect(result).to eq([3, 2, 1]) }
       end
 
-      it 'should order by comment content desc' do
-        expect(User.order_by(comments: :desc).pluck(:id)).to eq([1, 2, 3])
+      context 'order by name-asc' do
+        let(:order_by) { 'comments-asc' }
+        specify { expect(result).to eq([3, 2, 1]) }
       end
 
-      it 'should not change base query' do
-        query = User.where(name: 'example')
-        query.order_by(comments: :desc).pluck(:id)
-        expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+      context 'order by name: :desc' do
+        let(:order_by) { { comments: :desc } }
+        specify { expect(result).to eq([1, 2, 3]) }
+      end
+
+      context 'order by name-desc' do
+        let(:order_by) { 'comments-desc' }
+        specify { expect(result).to eq([1, 2, 3]) }
+      end
+
+      context 'base query immutability' do
+        it 'should not change base query' do
+          query = base_query.where(name: 'example')
+          query.order_by(comments: :desc).pluck(:id)
+          expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+        end
       end
     end
 
     context 'query' do
-      it 'should order by comment content asc' do
-        expect(User.order_by(custom_query: :asc).pluck(:email)).to eq(%w(example@example.com custom_example@example.com))
-        expect(User.order_by('custom_query-asc').pluck(:email)).to eq(%w(example@example.com custom_example@example.com))
+      let(:base_query) { User }
+      let(:order) { base_query.order_by(order_by) }
+      let(:result) { order.pluck(:email) }
+
+      context 'order by name: :asc' do
+        let(:order_by) { { custom_query: :asc } }
+        specify { expect(result).to eq(%w(example@example.com custom_example@example.com)) }
       end
 
-      it 'should order by comment content desc' do
-        expect(User.order_by(custom_query: :desc).pluck(:email)).to eq(%w(custom_example@example.com example@example.com))
-        expect(User.order_by('custom_query-desc').pluck(:email)).to eq(%w(custom_example@example.com example@example.com))
+      context 'order by name-asc' do
+        let(:order_by) { 'custom_query-asc' }
+        specify { expect(result).to eq(%w(example@example.com custom_example@example.com)) }
       end
 
-      it 'should not change base query' do
-        query = User.where(name: 'example')
-        query.order_by('custom_query-desc').pluck(:email)
-        expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+      context 'order by name: :desc' do
+        let(:order_by) { { custom_query: :desc } }
+        specify { expect(result).to eq(%w(custom_example@example.com example@example.com)) }
+      end
+
+      context 'order by name-desc' do
+        let(:order_by) { 'custom_query-desc' }
+        specify { expect(result).to eq(%w(custom_example@example.com example@example.com)) }
+      end
+
+      context 'base query immutability' do
+        it 'should not change base query' do
+          query = base_query.where(name: 'example')
+          query.order_by('custom_query-desc').pluck(:email)
+          expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
+        end
       end
     end
 
     context 'default ordering' do
       let(:spacing) { ENV['BUNDLE_GEMFILE'].include?('gemfiles/rails_4.gemfile') ? '  ' : ' ' }
-      let(:base_query_sql) { 'SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'' }
 
       context 'default proc' do
-        let(:result) { 'SELECT "users".* FROM "users" WHERE "users"."name" = \'example\''+spacing+'ORDER BY "users"."state"=\'active\' DESC,"users"."state"=\'pending\' DESC, users.name asc' }
+        let(:query) { User.where(name: 'example') }
 
-        specify do
-          query = User.where(name: 'example')
-          expect(query.order_by(nil).to_sql).to eq(result)
-          expect(query.to_sql).to eq(base_query_sql)
+        it 'should return asc ordering for nil' do
+          expect(query.order_by(nil).to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'' + spacing + 'ORDER BY "users"."state"=\'active\' DESC,"users"."state"=\'pending\' DESC, users.name asc')
+        end
+
+        it 'should not mutate base query' do
+          expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
         end
       end
 
       context 'default field direction' do
-        let(:result) { 'SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'' + spacing + 'ORDER BY users.name asc' }
+        let(:query) { User.where(name: 'example') }
 
-        specify do
-          query = User.where(name: 'example')
-          expect(query.order_by(:name).to_sql).to eq(result)
-          expect(query.to_sql).to eq(base_query_sql)
+        it 'should return asc ordering for :field' do
+          expect(query.order_by(:name).to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'' + spacing + 'ORDER BY users.name asc')
+        end
+
+        it 'should not mutate base query' do
+          expect(query.to_sql).to eq('SELECT "users".* FROM "users" WHERE "users"."name" = \'example\'')
         end
       end
 
       context 'default ordering' do
         let(:base_query_sql) { 'SELECT "comments".* FROM "comments" WHERE "comments"."user_id" = 1' }
+        let(:query) { Comment.where(user_id: 1) }
 
-        specify do
-          query = Comment.where(user_id: 1)
+        it 'should return asc ordering for \'\'' do
           expect(query.order_by('').to_sql).to eq(base_query_sql)
+        end
+
+        it 'should not mutate base query' do
           expect(query.to_sql).to eq(base_query_sql)
         end
       end
